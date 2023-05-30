2d2e464e1e84f9773f9ac7ed0befd68c83c27a38
Chima Ejiofor <mobiblunt@gmail.com>
2023-05-30 16:17:56 +0100

    on it

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router } from "react-router-dom";
+
+import NavBar from "./components/layout/NavBar";
+import Footer from "./components/layout/Footer";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      
+      
+    <div className="flex flex-col justify-between h-screen">
+    <NavBar/>
+    <main>
+    content
+
+    </main>
+    <Footer/>
     </div>
+
+
+    </Router>
   );
 }
 
