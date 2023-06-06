9b2819197de0be6e1488dd0673cedf46d5033d29
Chima Ejiofor <mobiblunt@gmail.com>
2023-06-06 04:41:49 +0100

    Fleshing out

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import NavBar from "./components/layout/NavBar";
 import Footer from "./components/layout/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+import { GithubProvider } from './context/github/GithubContext'
 
 
 function App() {
   return (
+    <GithubProvider>
     <Router>
       
       
     <div className="flex flex-col justify-between h-screen">
     <NavBar/>
     <main>
-    content
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/about" element={<About />}></Route>
+      <Route path="/*" element={<NotFound />}></Route>
+    </Routes>
 
     </main>
     <Footer/>
@@ -20,6 +29,7 @@ function App() {
 
 
     </Router>
+    </GithubProvider>
   );
 }
 
